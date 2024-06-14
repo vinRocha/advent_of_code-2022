f9baa5e0d5df4f7ea74c8be573375a7d796b5211
Vinicius R. da Silva <silva.viniciusr@gmail.com>
2024-06-14 14:56:33 -0300

    Small refactor in day2 cpp code.

--- challenges/02/c01/adv_2022_02_01.cpp
+++ challenges/02/c01/adv_2022_02_01.cpp
@@ -4,107 +4,119 @@
  *  Created on: 02 Dec 2022
  *      Author: vsilva1
  */
-#include <cstdlib>
+
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <string>
 
 #define INPUT_FILE "challenges/02/input/vrs_02.txt"
-std::fstream input_file;
 
-int read_ok() {
-    if (input_file.bad()) {
-        std::cerr << "Error reading from " << INPUT_FILE << "\n";
-        return -2;
-    } else if (input_file.eof()) {
+enum {
+    READ_OK,
+    READ_EOF,
+    READ_FAIL,
+    READ_BAD
+};
+
+int read_status(std::fstream &input_file) {
+    if (input_file.eof()) {
         std::cerr << "EOF: "<< INPUT_FILE << "\n";
-        return -1;
-    } else if (input_file.fail()) {
-        std::cerr << "Fail to read input to variable." << "\n";
-        return 0;
+        return READ_EOF;
     }
-    return 1;
+    else if (input_file.bad()) {
+        std::cerr << "Error reading from " << INPUT_FILE << "\n";
+        return READ_BAD;
+    }
+    else if (input_file.fail()) {
+        std::cerr << "Fail to read input to variable." << "\n";
+        return READ_FAIL;
+    }
+    return READ_OK;
 }
+
+int play_error(char, char);
 
 int main(int argc, char **argv) {
 
-    std::string s; //string buffer to read input file
-    std::vector<int> opponents; //opponents' play
-    std::vector<int> mine; //my play
+    std::fstream input_file;
+    std::string buffer; //string buffer to read input file
+    std::vector<char> player1; //my play
+    std::vector<char> player2; //opponents' play
     std::vector<int> my_score; //my_score
     int answer = 0;
+    int rc;
 
     input_file.open(INPUT_FILE, std::ios_base::in); //open file in read-only mode;
-    if (read_ok() < 1) {
+    rc = read_status(input_file);
+    if (rc > READ_OK) {
         std::cerr << "Error opening " << INPUT_FILE << "\n";
         input_file.close();
-        return -1;
+        return -rc;
     }
 
     //store play for each player given by input_file:
-    while (true) {
-        input_file >> s;
-        if (read_ok() < 1) { //eofbit || badbit || failbit
-            input_file.close();
-            break;
+    input_file >> buffer;
+    rc = read_status(input_file);
+    while (rc < READ_EOF) {
+        if (*buffer.c_str() > 'D') {
+            player1.push_back(*buffer.c_str());
         }
-        switch (*s.c_str()) {
-        case 'A':
-            opponents.push_back(1);
-            break;
-        case 'B':
-            opponents.push_back(2);
-            break;
-        case 'C':
-            opponents.push_back(3);
-            break;
-        case 'X':
-            mine.push_back(1);
-            break;
-        case 'Y':
-            mine.push_back(2);
-            break;
-        case 'Z':
-            mine.push_back(3);
-            break;
-        default:
-            std::cerr << "Invalid input: " << s << "\n";
+        else if (*buffer.c_str() < 'D') {
+            player2.push_back(*buffer.c_str());
         }
+        else {
+            std::cerr << "Invalid input: " << buffer << "\n";
+        }
+        input_file >> buffer;
+        rc = read_status(input_file);
+    }
+    input_file.close();
+
+    if (rc > READ_EOF) {
+        return -rc;
     }
 
-    if (opponents.size() != mine.size()){
+    if (player1.size() != player2.size()){
         std::cerr << "Error in vector sizes.\n";
-        return -1;
+        return 2;
     }
 
     //compute scores:
-    std::vector<int>::iterator it1 = mine.begin();
-    std::vector<int>::iterator it2 = opponents.begin();
-    while (it1 != mine.end()){
+    std::vector<char>::iterator it1 = player1.begin();
+    std::vector<char>::iterator it2 = player2.begin();
+    while (it1 != player1.end()){
         switch (*it1) {
-        case 1:
-            if (*it2 == 1) my_score.push_back(4);
-            else if (*it2 == 2) my_score.push_back(1);
-            else my_score.push_back(7);
+        case 'X':
+            if (*it2 == 'A') my_score.push_back(4);
+            else if (*it2 == 'B') my_score.push_back(1);
+            else if (*it2 == 'C')my_score.push_back(7);
+            else return play_error(*it2, *it1);
             break;
-        case 2:
-            if (*it2 == 2) my_score.push_back(5);
-            else if (*it2 == 3) my_score.push_back(2);
-            else my_score.push_back(8);
+        case 'Y':
+            if (*it2 == 'B') my_score.push_back(5);
+            else if (*it2 == 'C') my_score.push_back(2);
+            else if (*it2 == 'A') my_score.push_back(8);
+            else return play_error(*it2, *it1);
             break;
-        case 3:
-            if (*it2 == 3) my_score.push_back(6);
-            else if (*it2 == 1) my_score.push_back(3);
-            else my_score.push_back(9);
+        case 'Z':
+            if (*it2 == 'C') my_score.push_back(6);
+            else if (*it2 == 'A') my_score.push_back(3);
+            else if (*it2 == 'B') my_score.push_back(9);
+            else return play_error(*it2, *it1);
             break;
         default:
-            std::cerr << "Error in vector contents.\n";
-            return -1;
+            return play_error(*it2, *it1);
         }
         it1++; it2++; answer += my_score.back();
     }
 
     std::cout << "answer: " << answer << std::endl;
     return 0;
+
+}
+
+int play_error(char play1, char play2) {
+    std::cerr << "Invalid play: " << play1 << ',' << play2 << "\n";
+    return 2;
 }
--- challenges/02/c02/adv_2022_02_02.cpp
+++ challenges/02/c02/adv_2022_02_02.cpp
@@ -4,107 +4,118 @@
  *  Created on: 02 Dec 2022
  *      Author: vsilva1
  */
-#include <cstdlib>
+
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <string>
 
 #define INPUT_FILE "challenges/02/input/vrs_02.txt"
-std::fstream input_file;
 
-int read_ok() {
-    if (input_file.bad()) {
-        std::cerr << "Error reading from " << INPUT_FILE << "\n";
-        return -2;
-    } else if (input_file.eof()) {
+enum {
+    READ_OK,
+    READ_EOF,
+    READ_FAIL,
+    READ_BAD
+};
+
+int read_status(std::fstream &input_file) {
+    if (input_file.eof()) {
         std::cerr << "EOF: "<< INPUT_FILE << "\n";
-        return -1;
-    } else if (input_file.fail()) {
-        std::cerr << "Fail to read input to variable." << "\n";
-        return 0;
+        return READ_EOF;
     }
-    return 1;
+    else if (input_file.bad()) {
+        std::cerr << "Error reading from " << INPUT_FILE << "\n";
+        return READ_BAD;
+    }
+    else if (input_file.fail()) {
+        std::cerr << "Fail to read input to variable." << "\n";
+        return READ_FAIL;
+    }
+    return READ_OK;
 }
+
+int play_error(char, char);
 
 int main(int argc, char **argv) {
 
-    std::string s; //string buffer to read input file
-    std::vector<int> results; //game results
-    std::vector<int> mine; //my play
+    std::fstream input_file;
+    std::string buffer; //string buffer to read input file
+    std::vector<char> player2; //Oponent's play
+    std::vector<char> results; //game_result
     std::vector<int> my_score; //my_score
     int answer = 0;
+    int rc;
 
     input_file.open(INPUT_FILE, std::ios_base::in); //open file in read-only mode;
-    if (read_ok() < 1) {
+    rc = read_status(input_file);
+    if (rc > READ_OK) {
         std::cerr << "Error opening " << INPUT_FILE << "\n";
         input_file.close();
-        return -1;
+        return -rc;
     }
 
     //store play for each player given by input_file:
-    while (true) {
-        input_file >> s;
-        if (read_ok() < 1) { //eofbit || badbit || failbit
-            input_file.close();
-            break;
+    input_file >> buffer;
+    rc = read_status(input_file);
+    while (rc < READ_EOF) {
+        if (*buffer.c_str() > 'D') {
+            results.push_back(*buffer.c_str());
         }
-        switch (*s.c_str()) {
-        case 'A':
-            results.push_back(1);
-            break;
-        case 'B':
-            results.push_back(2);
-            break;
-        case 'C':
-            results.push_back(3);
-            break;
-        case 'X':
-            mine.push_back(1);
-            break;
-        case 'Y':
-            mine.push_back(2);
-            break;
-        case 'Z':
-            mine.push_back(3);
-            break;
-        default:
-            std::cerr << "Invalid input: " << s << "\n";
+        else if (*buffer.c_str() < 'D') {
+            player2.push_back(*buffer.c_str());
         }
+        else {
+            std::cerr << "Invalid input: " << buffer << "\n";
+        }
+        input_file >> buffer;
+        rc = read_status(input_file);
+    }
+    input_file.close();
+
+    if (rc > READ_EOF) {
+        return -rc;
     }
 
-    if (results.size() != mine.size()){
+    if (player2.size() != results.size()){
         std::cerr << "Error in vector sizes.\n";
-        return -1;
+        return 2;
     }
 
     //compute scores:
-    std::vector<int>::iterator it1 = mine.begin();
-    std::vector<int>::iterator it2 = results.begin();
-    while (it1 != mine.end()){
+    std::vector<char>::iterator it1 = player2.begin();
+    std::vector<char>::iterator it2 = results.begin();
+    while (it1 != player2.end()){
         switch (*it1) {
-        case 1:
-            if (*it2 == 1) my_score.push_back(3);
-            else if (*it2 == 2) my_score.push_back(1);
-            else my_score.push_back(2);
+        case 'A':
+            if (*it2 == 'X') my_score.push_back(0+3);
+            else if (*it2 == 'Y') my_score.push_back(3+1);
+            else if (*it2 == 'Z') my_score.push_back(6+2);
+            else return play_error(*it1, *it2);
             break;
-        case 2:
-            if (*it2 == 1) my_score.push_back(4);
-            else if (*it2 == 2) my_score.push_back(5);
-            else my_score.push_back(6);
+        case 'B':
+            if (*it2 == 'X') my_score.push_back(0+1);
+            else if (*it2 == 'Y') my_score.push_back(3+2);
+            else if (*it2 == 'Z') my_score.push_back(6+3);
+            else return play_error(*it1, *it2);
             break;
-        case 3:
-            if (*it2 == 1) my_score.push_back(8);
-            else if (*it2 == 2) my_score.push_back(9);
-            else my_score.push_back(7);
+        case 'C':
+            if (*it2 == 'X') my_score.push_back(0+2);
+            else if (*it2 == 'Y') my_score.push_back(3+3);
+            else if (*it2 == 'Z') my_score.push_back(6+1);
+            else return play_error(*it1, *it2);
             break;
         default:
-            std::cerr << "Error in vector contents.\n";
-            return -1;
+            return play_error(*it1, *it2);
         }
         it1++; it2++; answer += my_score.back();
     }
 
     std::cout << "answer: " << answer << std::endl;
     return 0;
+}
+
+int play_error(char play1, char play2) {
+    std::cerr << "Invalid play: " << play1 << ',' << play2 << "\n";
+    return 2;
 }
